e2d2c47fea5fa481a3aa47e9b28879dd1c5b9583
Rutika-123 <rutikanaik285@gmail.com>
2025-11-03 15:14:00 +0530

    Add files via upload

---
+++ front-end/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="container mt-4">
+      <h1 className="text-center mb-4">Task Tracker with Analytics</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
